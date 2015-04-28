08ce7bc8336ec3bb285e678a9439b44d390d74c7
fans656 <fans656@yahoo.com>
2015-04-28 21:40:33 +0800

    Add pause/stop.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import Queue
 import time
 import os
+import threading
 
 from PySide.QtGui import *
 from PySide.QtCore import *
@@ -10,6 +11,12 @@ from scanner import Scanner
 
 class Widget(QDialog):
 
+    SCAN_BUTTON_TEXT = '&Scan'
+    SCAN_BUTTON_PAUSE_TEXT = '&Pause'
+    SCAN_BUTTON_RESUME_TEXT = '&Resume'
+    INIT_PATH = 'E:/Movie'
+    #INIT_PATH = '.'
+
     def __init__(self, parent=None):
         super(Widget, self).__init__(parent)
         self.setWindowTitle('File Scanner')
@@ -17,16 +24,17 @@ class Widget(QDialog):
         self.scanningList = QTextEdit()
         self.dirEdit = QLineEdit()
         self.openButton = QPushButton('&Open')
-        self.scanButton = QPushButton('&Scan')
-        # TODO: add pause/stop
-        #self.stopButton = QPushButton('Stop')
+        self.scanButton = QPushButton(self.SCAN_BUTTON_TEXT)
+        self.stopButton = QPushButton('Stop')
 
         self.scanningList.setReadOnly(True)
         self.dirEdit.setEnabled(False)
         self.scanButton.setEnabled(False)
+        self.stopButton.setEnabled(False)
 
         self.openButton.clicked.connect(self.openDir)
-        self.scanButton.clicked.connect(self.scanDir)
+        self.scanButton.clicked.connect(self.onScanButtonClicked)
+        self.stopButton.clicked.connect(self.stopScan)
 
         lt = QVBoxLayout()
         lt.addWidget(self.scanningList)
@@ -35,18 +43,29 @@ class Widget(QDialog):
         lt_.addWidget(self.dirEdit)
         lt_.addWidget(self.openButton)
         lt_.addWidget(self.scanButton)
+        lt_.addWidget(self.stopButton)
         lt.addLayout(lt_)
 
         self.setLayout(lt)
+
+        self.controlEvent = threading.Condition()
+        self.status = ['stopped']
 
         self.idleTimer = QTimer()
         self.idleTimer.setInterval(0)
         self.idleTimer.timeout.connect(self.onIdle)
 
-        self.openDir(QDir('E:/Books').canonicalPath())
+        if not os.path.exists(self.INIT_PATH):
+            self.INIT_PATH = '.'
+        self.openDir(QDir(self.INIT_PATH).canonicalPath())
 
-        self.idleI = 0
-        self.totFiles = 0
+    def onScanButtonClicked(self):
+        if self.status[0] == 'stopped':
+            self.scanDir()
+        elif self.status[0] == 'running':
+            self.pauseScan()
+        elif self.status[0] == 'paused':
+            self.resumeScan()
 
     def openDir(self, path=None):
         if path is None:
@@ -59,29 +78,51 @@ class Widget(QDialog):
         self.scanningList.clear()
         path = self.dirEdit.text()
 
-        self.scanner = Scanner(path)
+        # construct a new scanner every time
+        # so we won't bother ungot items if we do a premature stop
+        self.scanner = Scanner(path, self.controlEvent, self.status)
         self.fpathsQueue = self.scanner.queue
+
+        self.status[0] = 'running'
+        self.scanButton.setText(self.SCAN_BUTTON_PAUSE_TEXT)
+        self.stopButton.setEnabled(True)
 
         self.idleTimer.start()
         self.scanner.start()
 
-        self.beg = time.time()
+    def pauseScan(self):
+        with self.controlEvent:
+            self.status[0] = 'paused'
+            self.idleTimer.stop()
+        self.scanButton.setText(self.SCAN_BUTTON_RESUME_TEXT)
+
+    def resumeScan(self):
+        with self.controlEvent:
+            self.status[0] = 'running'
+            self.idleTimer.start()
+            self.controlEvent.notifyAll()
+        self.scanButton.setText(self.SCAN_BUTTON_PAUSE_TEXT)
+
+    def stopScan(self):
+        # stop idle so we won't get item from the fpathQueue anymore
+        self.idleTimer.stop()
+        # trigger the event
+        with self.controlEvent:
+            self.status[0] = 'stopped'
+        # set ui state
+        self.scanButton.setText(self.SCAN_BUTTON_TEXT)
+        self.stopButton.setEnabled(False)
 
     def onIdle(self):
         try:
             for _ in xrange(10):
                 fpath = self.fpathsQueue.get(False)
                 if fpath is None:
-                    self.idleTimer.stop()
-                    elapsed = time.time() - self.beg
-                    s = 'scan finished. {} files in {} onIdle, in {:.1f}s'.format(self.totFiles, self.idleI, elapsed)
-                    QMessageBox.information(self, 'Result', s)
+                    self.stopScan()
                     break
                 self.scanningList.append(fpath)
-                self.totFiles += 1
         except Queue.Empty:
             pass
-        self.idleI += 1
 
 app = QApplication([])
 w = Widget()
--- scanner.py
+++ scanner.py
@@ -1,19 +1,30 @@
 import os
 import threading
 import Queue
+import time
 
 class Scanner(threading.Thread):
 
-    def __init__(self, path, start=False):
+    def __init__(self, path, controlEvent, status):
         super(Scanner, self).__init__()
         self.path = path
         self.queue = Queue.Queue()
-        if start:
-            self.start()
+        self.controlEvent = controlEvent
+        self.status = status
 
     def run(self):
         for dirpath, dirnames, filenames in os.walk(self.path):
             for filename in filenames:
+                # check for pause/stop
+                with self.controlEvent:
+                    while True:
+                        if self.status[0] == 'paused':
+                            self.controlEvent.wait()
+                        elif self.status[0] == 'running':
+                            break
+                        elif self.status[0] == 'stopped':
+                            return
+                # report file path
                 fpath = os.path.join(dirpath, filename)
                 self.queue.put(fpath)
         self.queue.put(None)
