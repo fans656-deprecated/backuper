43eacd6e0fb6734724e7e172e324169ae269fd84
fans656 <fans656@yahoo.com>
2015-05-03 22:09:27 +0800

    Use itertools.count() to write knuth's algorithm prettier

--- layout.py
+++ layout.py
@@ -1,24 +1,23 @@
-def knuth_layout(root, depth=0, slot=[0]):
+import itertools
+
+def knuth_layout(root, depth=0, slot=itertools.count()):
     if root:
         knuth_layout(root.left, depth + 1, slot)
-        root.x = slot[0]
+        root.x = slot.next()
         root.y = depth
-        slot[0] += 1
         knuth_layout(root.right, depth + 1, slot)
 
-def preorder(root, depth=0, slot=[0]):
+def preorder(root, depth=0, slot=itertools.count()):
     if root:
         preorder(root.left, depth + 1, slot)
         preorder(root.right, depth + 1, slot)
-        root.x = slot[0]
+        root.x = slot.next()
         root.y = depth
-        slot[0] += 1
 
-def postorder(root, depth=0, slot=[0]):
+def postorder(root, depth=0, slot=itertools.count()):
     if root:
-        root.x = slot[0]
+        root.x = slot.next()
         root.y = depth
-        slot[0] += 1
         postorder(root.left, depth + 1, slot)
         postorder(root.right, depth + 1, slot)
 
