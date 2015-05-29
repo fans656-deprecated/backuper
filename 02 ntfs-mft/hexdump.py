4e8e9a0cf59e212d7f1b176efa05b5985a2d2972
fans656 <fans656@yahoo.com>
2015-05-29 18:25:50 +0800

    Rewrite hexdump
    Now it's more beautiful

--- 02 ntfs-mft/hexdump.py
+++ 02 ntfs-mft/hexdump.py
@@ -1,8 +1,28 @@
+#def hexdump(data):
+#    groupJoin = lambda ch, size, a: [ch.join(a[i:i+size])
+#            for i in range(0, len(a), size)]
+#    make = lambda c1, c2, a: groupJoin(c1, 2, groupJoin(c2, 8, a))
+#    hs = ['{:02X}'.format(ord(c)) for c in data]
+#    cs = [c if 0x20 <= ord(c) < 0x7f else '.' for c in data]
+#    hs = make('  ', ' ', hs)
+#    cs = make(' ', '', cs)
+#    for i, (a, b) in enumerate(zip(hs, cs)):
+#        print '{:010X}: {:48}  {:16}'.format(i * 16, a, b)
+
+def group(a, *ns):
+    r = a
+    for n in ns:
+        r = [r[i:i+n] for i in xrange(0, len(r), n)]
+    return r
+
+def join(a, *cs):
+    return [cs[0].join(join(t, *cs[1:])) for t in a] if cs else a
+
 def hexdump(data):
     toHex = lambda c: '{:02X}'.format(ord(c))
-    toChar = lambda c: c if 0x20 <= ord(c) < 0x7f else '.'
-    group = lambda a, n, c: [c.join(a[i:i+n]) for i in xrange(0, len(a), n)]
-    make = lambda a, f, c1, c2: group(group(map(f, a), 8, c1), 2, c2)
-    maked = lambda r: zip(make(r, toHex, ' ', '  '), make(r, toChar, '', ' '))
-    fmt = '{:010X}: {:48}  {:16}'
-    return [fmt.format(i * 16, a, b) for i, (a, b) in enumerate(maked(data))]
+    toChr = lambda c: c if 32 <= ord(c) < 127 else '.'
+    make = lambda f, *cs: join(group(map(f, data), 8, 2), *cs)
+    hs = make(toHex, '  ', ' ')
+    cs = make(toChr, ' ', '')
+    for i, (h, c) in enumerate(zip(hs, cs)):
+        print '{:010X}: {:48}  {:16}'.format(i * 16, h, c)
--- 02 ntfs-mft/main.py
+++ 02 ntfs-mft/main.py
@@ -1,26 +1,9 @@
-import struct
-import os
-
 from hexdump import hexdump
 
-MBR_PTE_OFFSET = 0x1be # partition table entry offset in mbr
-MBR_PTE_SIZE = 16 # partion table entry size
-MBR_PTE_PBS_OFFSET = 0x08 # partion boot sector offset in partion entry
-
-def getPBSOffset(mbr):
-    i = MBR_PTE_OFFSET + MBR_PTE_PBS_OFFSET
-    return struct.unpack('<L', mbr[i:i+4])
-
-#with open('mbr0.bin', 'rb') as f:
-#    mbr = f.read(512)
-#    pbsOffset = 512 * getPBSOffset(mbr)[0]
-#    print pbsOffset
-
-#with open(r'\\.\PhysicalDrive0', 'rb') as f:
-#    f.seek(pbsOffset)
-#    pbs = f.read(512)
-#    open('pbs0-0.bin', 'wb').write(pbs)
-
-pbs = open('pbs0-0.bin', 'rb').read()
-for line in hexdump(pbs):
-    print line
+try:
+    f = open(r'\\.\PhysicalDrive0', 'rb')
+    s = f.read(512)
+    hexdump(s)
+    f.close()
+except IOError as e:
+    print dir(e)
